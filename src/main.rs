f99592168290017ff74f338248db93caf57bdef6
Abyssal Wolfe <wolfe@the.abyssal.one>
2023-02-23 02:53:50 -0500

    Fixed formatting in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
-use vulkano::{VulkanLibrary, instance::{Instance, InstanceCreateInfo}};
-use winit::{event_loop::EventLoop, window::WindowBuilder, event::{WindowEvent, Event}};
+use vulkano::{
+	instance::{Instance, InstanceCreateInfo},
+	VulkanLibrary,
+};
+use winit::{
+	event::{Event, WindowEvent},
+	event_loop::EventLoop,
+	window::WindowBuilder,
+};
 
 fn main() {
-    let library = VulkanLibrary::new().expect("No local Vulkan Library/DLL!");
-    let required_extensions = vulkano_win::required_extensions(&library);
-    let instance = Instance::new(
-        library,
-        InstanceCreateInfo {
-            enabled_extensions: required_extensions,
-            ..Default::default()
-        }
-    )
-    .expect("Failed to Create instance!");
+	let library = VulkanLibrary::new().expect("No local Vulkan Library/DLL!");
+	let required_extensions = vulkano_win::required_extensions(&library);
+	let instance = Instance::new(
+		library,
+		InstanceCreateInfo {
+			enabled_extensions: required_extensions,
+			..Default::default()
+		},
+	)
+	.expect("Failed to Create instance!");
 
-    let event_loop = EventLoop::new();
-    let surface = WindowBuilder::new().build(&event_loop).unwrap();
+	let event_loop = EventLoop::new();
+	let surface = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                println!("Program closed");
-                control_flow.set_exit();
-            },
-            _ => ()
-        }
-    });
+	event_loop.run(move |event, _, control_flow| match event {
+		Event::WindowEvent {
+			event: WindowEvent::CloseRequested,
+			..
+		} => {
+			println!("Program closed");
+			control_flow.set_exit();
+		}
+		_ => (),
+	});
 }
